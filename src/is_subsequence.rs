fe9949de1f3c22d35ac19353be2111808c26266d
lalit <labauskar@gmail.com>
2024-04-16 21:13:51 -0500

    Is Subsequence

---
+++ src/is_subsequence.rs
@@ -0,0 +1,15 @@
+pub fn is_subsequence(s: String, t: String) -> bool {
+    let mut res = false;
+    let mut s_it = s.chars().peekable();
+    for c in t.chars() {
+        if let Some(s_c) = s_it.peek() {
+            if s_c == &c {
+                _ = s_it.next();
+            }
+        } else {
+            break;
+        }
+    }
+
+    return s_it.peek().is_none();
+}
